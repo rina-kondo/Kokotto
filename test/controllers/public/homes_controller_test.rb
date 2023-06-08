0c0aadc6ad3cf84c13eb7a65d9568b237c749cb0
rina-kondo <122541296+rina-kondo@users.noreply.github.com>
2023-06-08 22:17:40 +0000

    fix: homes controller

--- test/controllers/public/homes_controller_test.rb
+++ test/controllers/public/homes_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Public::HomeControllerTest < ActionDispatch::IntegrationTest
+class Public::HomesControllerTest < ActionDispatch::IntegrationTest
   test "should get top" do
     get public_home_top_url
     assert_response :success
