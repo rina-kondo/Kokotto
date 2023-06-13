8558670d22bfe15c44fbc669729aca0568278c6d
rina-kondo <122541296+rina-kondo@users.noreply.github.com>
2023-06-13 04:17:18 +0000

    add display-select-tag.js

---
+++ app/javascript/display-select-tag.js
@@ -0,0 +1,77 @@
+document.addEventListener("DOMContentLoaded", setup);
+
+function setup() {
+  setupCategoryDisplay();
+}
+
+function setupCategoryDisplay() {
+  const buttons = document.querySelectorAll('.category');
+  buttons.forEach(button => {
+    button.addEventListener('click', () => handleCategoryClick(button.innerText));
+  });
+}
+
+async function handleCategoryClick(category) {
+  updateDisplay(category);
+  const imagePaths = await fetchImagePaths(category);
+  displayImages(imagePaths);
+  setupTagSelection();
+}
+
+function updateDisplay(category) {
+  const displayDiv = document.getElementById('display');
+  displayDiv.innerText = category;
+}
+
+async function fetchImagePaths(category) {
+  const response = await fetch(`/posts/image_paths?category=${encodeURIComponent(category)}`);
+  return await response.json();
+}
+
+function displayImages(imagePaths) {
+  const imageContainer = document.getElementById('imageContainer');
+  imageContainer.innerHTML = '';
+
+  imagePaths.forEach(imagePath => {
+    const img = createImageElement("/assets/" + imagePath, 100, 100);
+    img.classList.add(getBaseName(imagePath));
+    img.classList.add('tag');
+
+    imageContainer.appendChild(img);
+  });
+}
+
+function setupTagSelection() {
+  const images = document.querySelectorAll('.tag');
+
+  images.forEach(image => {
+    image.removeEventListener('click', handleTagClick);
+    image.addEventListener('click', handleTagClick);
+  });
+}
+
+function handleTagClick() {
+  const tagUrl = this.src;
+  displaySelectTag(tagUrl);
+}
+
+function displaySelectTag(tagUrl) {
+  const tagContainer = document.querySelector('#input-tag');
+  tagContainer.innerHTML = '';
+  const img = createImageElement(tagUrl, 100, 100);
+  tagContainer.appendChild(img);
+}
+
+function createImageElement(src, width, height) {
+  const img = document.createElement('img');
+  img.src = src;
+  img.alt = getBaseName(src);
+  img.width = width;
+  img.height = height;
+
+  return img;
+}
+
+function getBaseName(path) {
+  return path.split('/').pop().split('.')[0];
+}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "../geolocation"
-import "../display-taglist"
+import "../display-select-tag"
 
 Rails.start()
 Turbolinks.start()
