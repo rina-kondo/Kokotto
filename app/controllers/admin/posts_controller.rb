0ba934a786d33883c290f31b19eaac39b11d2d65
rina-kondo <122541296+rina-kondo@users.noreply.github.com>
2023-06-21 08:01:35 +0000

    fix admin-feature

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,4 +1,5 @@
 class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
   def destroy
     @post = Post.find(params[:id])
     @post.destroy ? (redirect_to request.referer) : (render admin_user_path, notice: "削除に失敗しました")
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
   def index
     @users = User.all
   end
@@ -10,27 +11,41 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    @posts = @user.posts.order(:created_at).reverse_order
+    @comments = @user.comments.order(:created_at).reverse_order
   end
 
   def update
-
+    if @user = User.update(user_params)
+      redirect_to request.referer, notice: "会員情報を編集しました"
+    else
+      render :edit
+    end
   end
 
   def deactivate
     @user = User.find(params[:id])
     if @user.update(is_deleted: true)
-      redirect_to request.referer, notice: "成功しました"
+      redirect_to request.referer, notice: "アカウントを無効にしました"
     else
-      redirect_to request.referer, alert: "失敗しました"
+      redirect_to request.referer, alert: "アカウントの無効化に失敗しました"
     end
   end
 
   def reactivate
     @user = User.find(params[:id])
     if @user.update(is_deleted: false)
-      redirect_to request.referer, notice: "成功しました"
+      redirect_to request.referer, notice: "アカウントを有効にしました"
     else
-      redirect_to request.referer, alert: "失敗しました"
+      redirect_to request.referer, alert: "アカウントの有効化に失敗しました"
     end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
 end
+
