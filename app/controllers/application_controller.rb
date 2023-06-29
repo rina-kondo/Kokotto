bd1e6e455b4652179dd5ce7941f9b149ad1d8349
rina-kondo <122541296+rina-kondo@users.noreply.github.com>
2023-06-29 11:51:41 +0000

    fix 404error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
-  rescue_from Exception, with: :render_500
 
   def after_sign_in_path_for(resource)
     posts_path
@@ -15,10 +14,6 @@ class ApplicationController < ActionController::Base
 
   def render_404
     render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
-  end
-
-  def render_500
-    render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
   end
 
   protected
